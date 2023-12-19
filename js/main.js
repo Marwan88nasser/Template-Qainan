298d573fe7ba1849ca49eff12faf304d9ca6cc86
Marwan88nasser <marwan.nasser565@gmail.com>
2023-12-19 14:04:55 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-// scroll to top btn 
-if(document.getElementById("up-btn")) {
+new Swiper(".swiper-landing", {
+  parallax: true,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 10,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// scroll to top btn
+if (document.getElementById("up-btn")) {
   const upBtn = document.getElementById("up-btn");
-  window.addEventListener("scroll", () => window.scrollY >= 300 ? upBtn.classList.add("show") : upBtn.classList.remove("show"));
-  upBtn.addEventListener("click", () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" }))
+  window.addEventListener("scroll", () =>
+    window.scrollY >= 300
+      ? upBtn.classList.add("show")
+      : upBtn.classList.remove("show")
+  );
+  upBtn.addEventListener("click", () =>
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+  );
 }
